09407859186063d63b88dd1de0f5f002727db9b2
Dominic Garms <djgarms@gmail.com>
2018-12-05 13:57:19 +0800

    make sure aws upload test works

--- tests/integration/file/awsUploadUrl.test.js
+++ tests/integration/file/awsUploadUrl.test.js
@@ -8,16 +8,16 @@ const externalImg = 'https://imgprxy.planet.training/v1/cj7r5j1a30jsl0132374v5q2
 
 
 test.serial('upload remote file with aws s3 sdk', async t => {
-    const headers = await getHead(externalImg)
-    const res = await awsUploadUrlToS3(externalImg)
-    const data = res.data
-    const newFileHeaders = await getHead(data.Location)
-    const deletedFiles = await deleteFilesFromS3([data.key])
-    t.is(deletedFiles.Deleted.length, 1)
-    t.is(!!data, true)
-    t.is(typeof data.Location, 'string')
-    t.is(typeof data.key, 'string')
-    t.is(headers['content-disposition'], newFileHeaders['content-disposition'])
-    t.is(headers['content-length'], newFileHeaders['content-length'])
-    t.is(headers['content-type'], newFileHeaders['content-type'])
+  const headers = await getHead(externalImg)
+  const res = await awsUploadUrlToS3(externalImg)
+  const data = res.data
+  const newFileHeaders = await getHead(data.Location)
+  const deletedFiles = await deleteFilesFromS3([data.key])
+  t.is(deletedFiles.Deleted.length, 1)
+  t.is(!!data, true)
+  t.is(typeof data.Location, 'string')
+  t.is(typeof data.key, 'string')
+  t.is(headers['content-disposition'], newFileHeaders['content-disposition'])
+  t.is(Number(headers['content-length']), Number(newFileHeaders['content-length']))
+  t.is(headers['content-type'], newFileHeaders['content-type'])
 })
