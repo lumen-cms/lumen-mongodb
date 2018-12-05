5d5ecf2c7783566ec1a4b0d82f05fd835bb630c7
Dominic Garms <djgarms@gmail.com>
2018-12-05 13:53:11 +0800

    fix number on file.test.js

--- tests/integration/file/file.test.js
+++ tests/integration/file/file.test.js
@@ -8,37 +8,43 @@ const externalImg = 'https://imgprxy.planet.training/v1/cj7r5j1a30jsl0132374v5q2
 
 
 test.serial('CRUD on file collection', async t => {
-    const headers = await getHead(externalImg)
-    const data = {
-        url: externalImg,
-        key: 'adfasfas',
-        name: 'test-img',
-        size: headers['content-length'],
-        width: 200,
-        height: 200,
-        contentType: headers['content-type']
-    }
-    // create
-    const {filesCreateOne} = await graphqlRequest(createFileGql, {data}, staticToken.moderator)
-    // update
-    const insertedId = filesCreateOne.insertedId
-    const {files} = await graphqlRequest(findFilesGql, {where: {id: {EQ: insertedId}}}, staticToken.moderator)
-    const {filesUpdatedOne} = await graphqlRequest(updateFileGql, {
-        where: {id: insertedId},
-        data: Object.assign({}, data, {name: 'new-name-img'})
-    }, staticToken.moderator)
-    const updatedFiles = await graphqlRequest(findFilesGql, {where: {id: {EQ: insertedId}}}, staticToken.moderator)
-        .then(r => r.files)
-    // delete
-    const {filesDeleteOne} = await graphqlRequest(deleteFileGql, {where: {id: insertedId}}, staticToken.moderator)
+  const headers = await getHead(externalImg)
+  const data = {
+    url: externalImg,
+    key: 'adfasfas',
+    name: 'test-img',
+    size: Number(headers['content-length']),
+    width: 200,
+    height: 200,
+    contentType: headers['content-type']
+  }
+  // create
+  const {filesCreateOne} = await graphqlRequest(createFileGql, {data}, staticToken.moderator)
+    .catch(e => {
+      if (e) {
+        throw new Error(e)
+      }
+    })
 
-    const firstCreateFile = files[0]
-    delete firstCreateFile.id
-    data.size = Number(data.size)
-    t.is(typeof insertedId, 'string')
-    t.is(filesDeleteOne.deletedCount, 1)
-    t.is(files.length, 1)
-    t.deepEqual(firstCreateFile, Object.assign({}, data, {_meta: null}))
-    t.is(updatedFiles.length, 1)
-    t.is(updatedFiles[0].name, 'new-name-img')
+  // update
+  const insertedId = filesCreateOne.insertedId
+  const {files} = await graphqlRequest(findFilesGql, {where: {id: {EQ: insertedId}}}, staticToken.moderator)
+  const {filesUpdatedOne} = await graphqlRequest(updateFileGql, {
+    where: {id: insertedId},
+    data: Object.assign({}, data, {name: 'new-name-img'})
+  }, staticToken.moderator)
+  const updatedFiles = await graphqlRequest(findFilesGql, {where: {id: {EQ: insertedId}}}, staticToken.moderator)
+    .then(r => r.files)
+  // delete
+  const {filesDeleteOne} = await graphqlRequest(deleteFileGql, {where: {id: insertedId}}, staticToken.moderator)
+
+  const firstCreateFile = files[0]
+  delete firstCreateFile.id
+  data.size = Number(data.size)
+  t.is(typeof insertedId, 'string')
+  t.is(filesDeleteOne.deletedCount, 1)
+  t.is(files.length, 1)
+  t.deepEqual(firstCreateFile, Object.assign({}, data, {_meta: null}))
+  t.is(updatedFiles.length, 1)
+  t.is(updatedFiles[0].name, 'new-name-img')
 })
